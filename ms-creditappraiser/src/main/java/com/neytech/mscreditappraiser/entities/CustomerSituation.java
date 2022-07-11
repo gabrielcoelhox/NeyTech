2457a08ab760df16b3979a5d3499bfeb83cfaf88
Gabriel Coelho <gabriel.coelhox@gmail.com>
2022-07-11 18:34:33 -0300

    Fixing bug in Credit Appraiser service

--- ms-creditappraiser/src/main/java/com/neytech/mscreditappraiser/entities/CustomerSituation.java
+++ ms-creditappraiser/src/main/java/com/neytech/mscreditappraiser/entities/CustomerSituation.java
@@ -2,6 +2,8 @@ package com.neytech.mscreditappraiser.entities;
 
 import java.util.List;
 
+import com.neytech.mscreditappraiser.clients.CardsPerCustomerResponse;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,5 +16,5 @@ import lombok.NoArgsConstructor;
 public class CustomerSituation {
 
 	private CustomerData customer;
-	private List<CustomerCardResponse> cards;
+	private List<CardsPerCustomerResponse> cards;
 }
